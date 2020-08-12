332d492355e8f95ea0d049d992606a4af557ea58
Raihan Mahmud (RAM) <mahmud.raihan@yahoo.com>
2020-08-12 16:42:53 +0600

    HelloWorld widget-plugin

---
+++ src/Plugins/Nop.Plugin.Widget.HelloWorld/Components/ExampleWidgetViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Nop.Web.Framework.Components;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nop.Plugin.Widget.HelloWorld.Components
+{
+    [ViewComponent(Name = "HelloWorldWidget")]
+    public class ExampleWidgetViewComponent : NopViewComponent
+    {
+        public IViewComponentResult Invoke(string widgetZone)
+        {
+            return Content("Hello World");
+        }
+    }
+}
---
+++ src/Plugins/Nop.Plugin.Widget.HelloWorld/HelloWorldPlugin.cs
@@ -0,0 +1,60 @@
+﻿using Nop.Services.Cms;
+using Nop.Services.Plugins;
+using System;
+using System.Collections.Generic;
+
+namespace Nop.Plugin.Widget.HelloWorld
+{
+    public class HelloWorldPlugin : BasePlugin, IWidgetPlugin
+    {
+        /// <summary>
+        /// Gets a value indicating whether to hide this plugin on the widget list page in the admin area
+        /// </summary>
+        public bool HideInWidgetList => false;
+
+        bool IWidgetPlugin.HideInWidgetList => throw new NotImplementedException();
+
+        /// <summary>
+        /// Gets widget zones where this widget should be rendered
+        /// </summary>
+        /// <returns>Widget zones</returns>
+        public string GetWidgetViewComponentName(string widgetZone)
+        {
+            return "HelloWorldWidget";
+        }
+
+        /// <summary>
+        /// Gets a name of a view component for displaying widget
+        /// </summary>
+        /// <param name="widgetZone">Name of the widget zone</param>
+        /// <returns>View component name</returns>
+        public IList<string> GetWidgetZones()
+        {
+            return new List<string> { "home_page_before_categories" };
+        }
+
+        public override void Install()
+        {
+            //Logic during installation goes here...
+
+            base.Install();
+        }
+
+        public override void Uninstall()
+        {
+            //Logic during uninstallation goes here...
+
+            base.Uninstall();
+        }
+
+        //string IWidgetPlugin.GetWidgetViewComponentName(string widgetZone)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //IList<string> IWidgetPlugin.GetWidgetZones()
+        //{
+        //    throw new NotImplementedException();
+        //}
+    }
+}
